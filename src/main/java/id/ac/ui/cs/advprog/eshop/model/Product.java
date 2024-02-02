7be464cfad3161ccf4b467b3a08bad4a3b6b003f
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-02-02 10:18:03 +0700

    Added Product.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
