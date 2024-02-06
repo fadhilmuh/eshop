43aa48c4bca1ed1b35bd158bbdf31e68e68dd0e2
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-02-06 22:23:09 +0700

    Added HomePage.java and HomePage.html for HomePageFunctionalTest.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePage.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePage {
+    
+    @GetMapping("/")
+    public String createProductPage(Model model){
+        return "homepage";
+    }
+
+}
